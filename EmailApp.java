86d99308cae59c1ff707bd19017fea39caa7bd3a
Asav7 <Albert.Asav7@gmail.com>
2018-02-12 15:25:24 +0100

    added java files

---
+++ EmailApp.java
@@ -0,0 +1,12 @@
+package emailapp;
+
+/**
+ * Created by Albert on 02.02.2018.
+ */
+public class EmailApp {
+    public static void main(String[] args) {
+        Email em1 = new Email("John", "Smith");
+        System.out.println(em1.showInfo());
+
+    }
+}
